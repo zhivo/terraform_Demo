51161fa2c55e5e622c65f5205173c229780badac
zhivo <158048058+zhivo@users.noreply.github.com>
2024-02-06 20:56:58 +0200

    commit

---
+++ main.tf
@@ -0,0 +1,87 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.89.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "azRG" {
+  name     = "${var.resource_group_name}${random_integer.ri.result}"
+  location = var.resource_group_location
+}
+
+resource "azurerm_service_plan" "azSP" {
+  name                = "${var.app_service_plan_name}-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.azRG.name
+  location            = azurerm_resource_group.azRG.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "azLWA" {
+  name                = "${var.app_service_name}-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.azRG.name
+  location            = azurerm_service_plan.azSP.location
+  service_plan_id     = azurerm_service_plan.azSP.id
+
+  site_config {
+    application_stack {
+      dotnet_version = "6.0"
+    }
+    always_on = false
+  }
+
+  connection_string{
+    name = "DefaultConnection"
+    type = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.azDBserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.azDBtable.name};User ID=${azurerm_mssql_server.azDBserver.administrator_login};Password=${azurerm_mssql_server.azDBserver.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+}
+
+resource "azurerm_mssql_server" "azDBserver" {
+  name                         = "${var.sql_server_name}${random_integer.ri.result}"
+  resource_group_name          = azurerm_resource_group.azRG.name
+  location                     = azurerm_resource_group.azRG.location
+  version                      = "12.0"
+  administrator_login          = var.sql_admin_login
+  administrator_login_password = var.sql_admin_password
+}
+
+resource "azurerm_mssql_database" "azDBtable" {
+  name           = "${var.sql_database_name}-${random_integer.ri.result}"
+  server_id      = azurerm_mssql_server.azDBserver.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 2
+  sku_name       = "S0"
+  zone_redundant = false
+}
+
+resource "azurerm_mssql_firewall_rule" "azFR" {
+  name             = "${var.firewall_rule_name}-${random_integer.ri.result}"
+  server_id        = azurerm_mssql_server.azDBserver.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+resource "azurerm_app_service_source_control" "azSSC" {
+  app_id                 = azurerm_linux_web_app.azLWA.id
+  repo_url               = var.repo_URL
+  branch                 = "main"
+  use_manual_integration = true
+}
+
---
+++ variables.tf
@@ -0,0 +1,50 @@
+variable "resource_group_name" {
+  type = string
+  description = "Resource group name"
+}
+
+variable "resource_group_location" {
+  type = string
+  description = "Resoutce group location"
+}
+
+variable "app_service_plan_name" {
+  type = string
+  description = "Service plan name"
+}
+
+
+variable "app_service_name" {
+  type = string
+  description = "Service name"
+}
+
+variable "sql_server_name" {
+  type = string
+  description = "SQL Server name"
+}
+
+variable "sql_database_name" {
+  type = string
+  description = "SQL database name"
+}
+
+variable "sql_admin_login" {
+  type = string
+  description = "SQL admin login"
+}
+
+variable "sql_admin_password" {
+  type = string
+  description = "SQL admin password"
+}
+
+variable "firewall_rule_name" {
+  type = string
+  description = "Firewall rule name"
+}
+
+variable "repo_URL" {
+  type = string
+  description = "repo URL"
+}
