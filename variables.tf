ac55e05fadbd1aaa15826aa23089e308ece523af
zhivo <158048058+zhivo@users.noreply.github.com>
2024-02-06 21:10:43 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,50 +1,50 @@
 variable "resource_group_name" {
-  type = string
+  type        = string
   description = "Resource group name"
 }
 
 variable "resource_group_location" {
-  type = string
+  type        = string
   description = "Resoutce group location"
 }
 
 variable "app_service_plan_name" {
-  type = string
+  type        = string
   description = "Service plan name"
 }
 
 
 variable "app_service_name" {
-  type = string
+  type        = string
   description = "Service name"
 }
 
 variable "sql_server_name" {
-  type = string
+  type        = string
   description = "SQL Server name"
 }
 
 variable "sql_database_name" {
-  type = string
+  type        = string
   description = "SQL database name"
 }
 
 variable "sql_admin_login" {
-  type = string
+  type        = string
   description = "SQL admin login"
 }
 
 variable "sql_admin_password" {
-  type = string
+  type        = string
   description = "SQL admin password"
 }
 
 variable "firewall_rule_name" {
-  type = string
+  type        = string
   description = "Firewall rule name"
 }
 
 variable "repo_URL" {
-  type = string
+  type        = string
   description = "repo URL"
 }
